79dab31a2a963c020fb100dd3ee59dea6b972144
Vic Chimenti <victor.chimenti@gmail.com>
2022-02-08 13:02:56 -0800

    rm example tags

--- v9-organizer-cejscourse.js
+++ v9-organizer-cejscourse.js
@@ -262,18 +262,6 @@
     } 
 
 
-    //  let imagePath = T4Utils.brokerUtils.generateT4Tag({
-    //     type: 'media',
-    //     id: imageId,
-    //     formatter: 'path/*'
-    // });
- 
-    var mediaPath =
-  BrokerUtils.processT4Tags (dbStatement, publishCache, section, galleryContent, language, isPreview,
-  '<t4 type="content" name="Gallery Image" output="normal" formatter="path/*" />');
-var mediaDescription =
-  BrokerUtils.processT4Tags (dbStatement, publishCache, section, galleryContent, language, isPreview, 
-  '<t4 type="content" name="Gallery Image" output="normal" formatter="description" />');
  
  
   
