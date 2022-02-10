29a4c80733281f58b362846801a7723262f96bbf
Vic Chimenti <victor.chimenti@gmail.com>
2022-02-10 13:00:47 -0800

    course name

--- v9-organizer-cejscourse.js
+++ v9-organizer-cejscourse.js
@@ -149,10 +149,9 @@
 
          contentName: getContentValues('<t4 type="content" name="Name" output="normal" modifiers="striptags,htmlentities" />'),
          articleTitle: getContentValues('<t4 type="content" name="Article Title" output="normal" modifiers="striptags,htmlentities" />'),
+         courseName: getContentValues('<t4 type="content" name="Course Name" output="normal" modifiers="striptags,htmlentities" />'),
          college: getContentValues('<t4 type="content" name="College" output="normal" modifiers="striptags,htmlentities" />'),
          academicLevel: getContentValues('<t4 type="content" name="Level" output="normal" modifiers="striptags,htmlentities" />'),
-
-
          primarySectionName: getContentValues('<t4 type="content" name="Primary Section Name" output="normal" modifiers="striptags,htmlentities" />'),
          subjectDescription: getContentValues('<t4 type="content" name="Subject" output="normal" modifiers="striptags,htmlentities" />'),
          icons: getContentValues('<t4 type="content" name="Icon ID" output="normal" modifiers="striptags,htmlentities" />'),
