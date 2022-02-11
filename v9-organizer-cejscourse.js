d46484cc57521d62e48b6d9267299c0a13962b45
Vic Chimenti <victor.chimenti@gmail.com>
2022-02-11 08:36:22 -0800

    rm dots

--- v9-organizer-cejscourse.js
+++ v9-organizer-cejscourse.js
@@ -9,7 +9,7 @@
      *
      *     Document will write once when the page loads
      *
-     *     @version 7.16.11
+     *     @version 7.16.12
      */
 
 
@@ -198,7 +198,7 @@
       * 
       * */
     let summaryString = (cejscDict.subjectDescription.content && cejscDict.articleTitle.content)
-                        ? '<p class="card-text shortSummary">' + cejscDict.summaryDescription.content + '<a href="' + cejscDict.fullTextLink.content + '" class="card-link" title="See the full course description: ' + cejscDict.articleTitle.content + '">...Read More >>></a></p>'
+                        ? '<p class="card-text shortSummary">' + cejscDict.summaryDescription.content + '... <a href="' + cejscDict.fullTextLink.content + '" class="card-link" title="See the full course description: ' + cejscDict.articleTitle.content + '">Read More >>></a></p>'
                         : '<span class="card-text shortSummary visually-hidden hidden">No valid summary provided</span>';
 
 
