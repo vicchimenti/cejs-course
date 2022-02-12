fbaf0b347ac8758665c004b0a1230aab1e6b30e1
Vic Chimenti <victor.chimenti@gmail.com>
2022-02-12 10:27:34 -0800

    rm cm cd

--- filter.js
+++ filter.js
@@ -19,19 +19,12 @@
 
 
 $(function () {
-    // After the DOM is ready, Wait until the window loads
+
     $(window).load(function () {
-        // Once window loads set a timeout delay
         setTimeout(function () {
-            console.log("timeout function");
-
-
-
-
-
-            //** global array holds list of content items that will render after filter selection **//
-            var visibleItems = [];
-            var parseItems = {};
+            
+            let visibleItems = [];
+            let parseItems = {};
 
 
 
@@ -39,17 +32,15 @@ $(function () {
             //   ***   Process and Parse Visible Items   ***   //
             $(function () {
                 let parseItemsToDisplay = function () {
-                    // assign array of currently visible content items
                     visibleItems = $('.cejscourseWrapper').not('.hideByText, .hideBySchool, .hideByLevel');
-                    // check to see if array is empty
+                    
                     if (visibleItems.length == 0) {
-                        // when array is empty show the results message
                         $('.noResultsToShow').removeClass('hideResultsMessage');
                     } else {
-                        // when array has content items suppress the results message
                         $('.noResultsToShow').addClass('hideResultsMessage');
                     }
                 };
+
                 parseItems.process = parseItemsToDisplay;
             });
 
@@ -58,18 +49,15 @@ $(function () {
 
             //   ***   Keyword Search   ***   //
             $(function () {
-                // scan the keyword each character the user inputs
                 $('#keystroke_filter').on('keyup', function () {
-                    // Assign Search Key
                     let keyword = $(this).val().toLowerCase();
-                    // filter the education abroad items for the input key
+
                     $(function () {
                         $('.cejscourseWrapper').filter(function () {
-                            // when the search key is not present in the item then hide the item
                             $(this).toggleClass('hideByText', !($(this).text().toLowerCase().indexOf(keyword) > -1));
                         });
                     });
-                    // parse out unselected content items and limit display to user selected items
+
                     parseItems.process();
                 });
             });
@@ -84,12 +72,10 @@ $(function () {
                     // Assign Search Key
                     let typeKey = $(this).val();
                     let viewAll = "All";
-                    console.log("typeKey: " + typeKey);
                     // If Search Key is Not Null then Compare to the Type List Items in Each Content Item
                     if (typeKey != viewAll) {
                         $('.college').filter(function (i, e) {
                             var typeValue = $(this).text();
-                            console.log("typeValue: " + typeValue);
                             // Check to see if the Key and Value are a Match
                             if (typeValue.match(typeKey)) {
                                 $(this).parents('.cejscourseWrapper').removeClass('hideBySchool');
@@ -113,15 +99,12 @@ $(function () {
             //   ***   Course Level Filter   ***   //
             $(function () {
                 $('form input:radio').change(function () {
-
                     let typeKey = $(this).val();
                     let viewAll = "All";
-                    console.log("typeKey: " + typeKey);
 
                     if (typeKey != viewAll) {
                         $('.academicLevel').filter(function (i, e) {
                             var typeValue = $(this).text();
-                            console.log("typeValue: " + typeValue);
 
                             if (typeValue.match(typeKey)) {
                                 $(this).parents('.cejscourseWrapper').removeClass('hideByLevel');
