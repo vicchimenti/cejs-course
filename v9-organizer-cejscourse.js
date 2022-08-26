8f7f175a4699ebd5c9a0bfcb7bcac2714521b3ed
Vic Chimenti <victor.chimenti@gmail.com>
2022-08-26 10:45:13 -0700

    rm fig caption

--- v9-organizer-cejscourse.js
+++ v9-organizer-cejscourse.js
@@ -110,7 +110,7 @@
          info.setInput(media);
  
          let mediaHTML = (info.check()) ?
-             '<figure class="figure"><img src="' + mediaPath + '" class="listgroupImage figure-img img-fluid" aria-label="' + mediaInfo.getName() + '" alt="' + mediaInfo.getDescription() + '" width="' + info.getWidth() + '" height="' + info.getHeight() + '" loading="auto" /></figure><figcaption class="figure-caption visually-hidden hidden">' + mediaInfo.getName() + '</figcaption>' :
+             '<figure class="figure"><img src="' + mediaPath + '" class="listgroupImage figure-img img-fluid" aria-label="' + mediaInfo.getName() + '" alt="' + mediaInfo.getDescription() + '" width="' + info.getWidth() + '" height="' + info.getHeight() + '" loading="auto" /></figure>' :
              '<span class="listgroupImage visually-hidden hidden">Invalid Image ID</span>';
  
          return mediaHTML;
