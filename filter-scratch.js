3d39a30467ef4eae5ca500ee6992b629dca53e07
Vic Chimenti <victor.chimenti@gmail.com>
2022-10-03 10:16:42 -0700

    trigger counter

--- filter-scratch.js
+++ filter-scratch.js
@@ -28,12 +28,13 @@ $(function () {
 
             
 
-            function countCourses () {
+            function countCourses() {
 
                 let countedCourses = document.querySelectorAll('article.cejscourseWrapper:not( .hideByText, .hideBySchool, .hideByLevel, .hideByGoal, .hideByLsap)');
                 console.log("countedCourses: " + countedCourses.length);
 
             }
+            countCourses();
 
 
             //   ***   Process and Parse Visible Items   ***   //
