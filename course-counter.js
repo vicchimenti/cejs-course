5767a91b9953db150cccfbcf4093b8f4bfe09413
Vic Chimenti <victor.chimenti@gmail.com>
2022-10-03 09:46:46 -0700

    log results

--- course-counter.js
+++ course-counter.js
@@ -5,3 +5,5 @@
 
 
  let countedCourses = document.querySelectorAll('article.cejscourseWrapper:not( .hideByText, .hideBySchool, .hideByLevel, .hideByGoal, .hideByLsap)');
+
+ console.log("countedCourses: " + countedCourses.length);
