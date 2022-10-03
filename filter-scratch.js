f06d01c80619ee2e060ac96f1a7c951c854707b5
Vic Chimenti <victor.chimenti@gmail.com>
2022-10-03 10:29:45 -0700

    ud

--- filter-scratch.js
+++ filter-scratch.js
@@ -33,6 +33,8 @@ $(function () {
                 let countedCourses = document.querySelectorAll('article.cejscourseWrapper:not( .hideByText, .hideBySchool, .hideByLevel, .hideByGoal, .hideByLsap)');
                 console.log("countedCourses: " + countedCourses.length);
 
+                course-count
+
             }
             countCourses();
 
